fdb48496dd4e42bc54c42fc3f4b22422feba7f54
Sudhir Kumar <sudhirkumar199888@gmail.com>
2025-08-26 10:53:59 +0530

    deleted all the files

--- routes/shopTypeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getShopTypes, addShopType } = require("../controllers/shopTypeController");
-
-router.get("/", getShopTypes);
-router.post("/", addShopType); // In production, protect this route
-
-module.exports = router;
